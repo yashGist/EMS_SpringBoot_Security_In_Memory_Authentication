4f23a04d66166cf567f48eccca61326dc361ea3c
Yashdeep <ramawatyashdeep@gmail.com>
2025-09-28 18:48:38 +0530

    added @EnableMEthodSecurity(prePostEnabled=true)

--- src/main/java/com/ems/config/WebSecurityConfiguration.java
+++ src/main/java/com/ems/config/WebSecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.ems.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
@@ -16,6 +17,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfiguration {
 
     @Bean
--- src/main/java/com/ems/controller/EmployeeController.java
+++ src/main/java/com/ems/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import com.ems.model.EmployeeModel;
 import com.ems.repository.EmployeeRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize; // 1. IMPORT ADDED
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,81 +18,89 @@ import java.util.List;
 
 @Controller
 public class EmployeeController {
-	
+
 	@Autowired
 	EmployeeRepository repository;
-	
+
 	@GetMapping("/index")
-	public String  getIndexPage() {
-		return  "index";
+	public String getIndexPage() {
+		return "index";
 	}
-	
+
+	@PreAuthorize("hasRole('MANAGER')")
 	@GetMapping("/addEmployee")
-	
 	public String getAddEmployeePage(Model model) {
-		EmployeeModel  empModel=new EmployeeModel();
+		EmployeeModel empModel = new EmployeeModel();
 		model.addAttribute("empModel", empModel);
 		return "addEmployee";
 	}
-	
+
+	@PreAuthorize("hasRole('MANAGER')")
 	@PostMapping("/saveEmployee")
-	public String  saveEmployee(@ModelAttribute("empModel")EmployeeModel  empModel, Model model) {
+	public String saveEmployee(@ModelAttribute("empModel") EmployeeModel empModel, Model model) {
 		Employee e = new Employee();
 		BeanUtils.copyProperties(empModel, e);
-		boolean flag=repository.existsById(e.getEmpno());
-		if(flag) {
+		boolean flag = repository.existsById(e.getEmpno());
+		if (flag) {
 			model.addAttribute("message", "Employee with given empno already exist");
-		}
-		else {
+		} else {
 			repository.save(e);
 			model.addAttribute("message", "Employee is added to Database");
 		}
 		return "index";
 	}
-	
+
 	@GetMapping("/listEmployees")
 	public String listEmployees(Model model) {
-		List<Employee>  empList= repository.findAll();
-		
-		List<EmployeeModel>  empModelList=new  ArrayList<>();
-		
+		List<Employee> empList = repository.findAll();
+
+		List<EmployeeModel> empModelList = new ArrayList<>();
+
 		empList.forEach(e -> {
-			EmployeeModel emodel=new EmployeeModel();
+			EmployeeModel emodel = new EmployeeModel();
 			BeanUtils.copyProperties(e, emodel);
 			empModelList.add(emodel);
 		});
 		model.addAttribute("empModelList", empModelList);
 		return "employeesList";
 	}
-	
+
+	@PreAuthorize("hasRole('MANAGER')")
 	@GetMapping("/editEmployee")
 	public String editEmployeePage(@RequestParam("id") int empno, Model model) {
-		Employee  e = repository.findById(empno).get();
-		EmployeeModel  emodel=new EmployeeModel();
+		Employee e = repository.findById(empno).get();
+		EmployeeModel emodel = new EmployeeModel();
 		BeanUtils.copyProperties(e, emodel);
-		model.addAttribute("emodel",emodel);
+		model.addAttribute("emodel", emodel);
 		return "editEmployee";
-	
+
 	}
+
+	@PreAuthorize("hasRole('MANAGER')")
 	@PostMapping("/updateEmployee")
 	public String updateEmployee(@ModelAttribute("emodel") EmployeeModel emodel) {
-		Employee e=new Employee();
+		Employee e = new Employee();
 		BeanUtils.copyProperties(emodel, e);
 		repository.saveAndFlush(e);
 		return "redirect:listEmployees";
 	}
-	
+
+	// 2. SECURITY ANNOTATION ADDED
+	@PreAuthorize("hasRole('MANAGER')")
 	@GetMapping("/deleteEmployee")
-	//@PreAuthorize("hasRole('ROLE_MANAGER')&&authentication.name=='anu'")
 	public String deleteEmployee(@RequestParam("id") int empno) {
 		repository.deleteById(empno);
 		return "redirect:listEmployees";
 	}
-	
+
 	@GetMapping("/loggedout")
 	public String userLoggedOut() {
 		return "logoutSuccess";
 	}
-	
 
+	// 3. NEW METHOD ADDED FOR THE CUSTOM ACCESS DENIED PAGE
+	@GetMapping("/access-denied")
+	public String getAccessDeniedPage() {
+		return "denied"; // Assumes you have a denied.jsp file
+	}
 }
